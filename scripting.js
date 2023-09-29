51836523d56c1880f6a419d592f1ef3513037665
PierceVDR <PierceVDR@gmail.com>
2023-09-29 16:56:23 -0400

    first commit

---
+++ scripting.js
@@ -0,0 +1,9 @@
+function initalize()
+{
+    areTheyCute = document.getElementById("areTheyCute");
+}
+function theyAreCute()
+{
+    areTheyCute.innerHTML = "Yes they are!";
+    console.log("Wow, those are some cute cats!");
+}
